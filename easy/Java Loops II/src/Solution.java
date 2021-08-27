db333d842a812071648e9f9c591f5e588d100d09
meeksjack95 <meeksjack95@gmail.com>
2021-08-27 14:53:51 -0600

    Java Loops II

--- easy/Java Loops II/src/Solution.java
+++ easy/Java Loops II/src/Solution.java
@@ -1,2 +1,24 @@
-public class Solution {
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+
+            int last = 0;
+            for(int j = 0; j < n; j++){
+                int result = a + (int)Math.pow(2, j) * b;
+                result = result + last;
+                last = result;
+                System.out.print(result + " ");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
 }
